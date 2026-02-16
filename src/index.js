8c809a8ab185579a2cffc319ff51b8520e35f3da
gemvibes <faisal.almahdibrata@gmail.com>
2026-02-16 21:55:58 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // Ini harus pakai 'A' besar supaya nyambung ke App.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
